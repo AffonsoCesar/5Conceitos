6c8409011e6872089c8b3ebfe7f1355c5e533e84
AffonsoCesar <affonso.souza@dce.ufpb.br>
2016-04-09 10:27:35 -0230

    Projeto Atualizado

--- app/src/main/java/com/example/affonso/cincoconceitos/MainActivity.java
+++ app/src/main/java/com/example/affonso/cincoconceitos/MainActivity.java
@@ -16,10 +16,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
-    String []lista = new String[] {"View","TextView","EditText", "Button", "CheckBox", "Toggle  Button"};
-    ArrayAdapter<?> adapter;
-    android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+    private ListView listView; //Criação de um listView do Tipo ListView.
+    String []lista = new String[] {"View","TextView","EditText", "Button", "CheckBox", "Toggle  Button"}; //Criação da lista que será carregada no ListView
+    ArrayAdapter<?> adapter; //ArrayAdapter para adaptação da lista no ListView
+    android.support.v4.app.FragmentManager fm = getSupportFragmentManager(); //Suporte para o uso de fragment
 
 
     @Override
@@ -27,20 +27,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        adapter = new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, lista);
-        listView = (ListView)findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        adapter = new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, lista); //Dando um new no adapter para preencher a listView
+        listView = (ListView)findViewById(R.id.listView); // associando o ListView ao componente da tela no main XML.
+        listView.setAdapter(adapter); //Setando a ListView com o adapter.
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) { //Controle de clique da ListView e associação dos fragments para a ListView
                 TextView txt = (TextView) view;
                 txt.setTextColor(Color.GRAY);
 
 
-                if (position == 0) {
+                if (position == 0) { //Condição para os cliques
                     Fragment f1 = new Fragment01();
-                    fm.beginTransaction().replace(R.id.ladodireito, f1).commit();
+                    fm.beginTransaction().replace(R.id.ladodireito, f1).commit(); //inflando o fragment na tela.
                 } else if (position == 1) {
                     Fragment f2 = new Fragment02();
                     fm.beginTransaction().replace(R.id.ladodireito, f2).commit();
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ActionBar actionBar = getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar(); //Edição da ActionBar
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(false);
             actionBar.setIcon(R.drawable.ic_launcher);
@@ -68,18 +68,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+    public boolean onCreateOptionsMenu(Menu menu) { //Criação do Menu
+        getMenuInflater().inflate(R.menu.menu_main, menu); //Infalndo o Menu
+        return super.onCreateOptionsMenu(menu); //Retorno do Menu
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) { //Funcionalidade do clique no item do menu
 
         int id = item.getItemId();
-
-        if(id == R.id.action_sobre){
-            Toast.makeText(this, "Projeto Final do Curso - Google Study Jams 2016 - Android for Beginners", Toast.LENGTH_LONG).show();
+        if(id == R.id.action_sobre){ //condição do clique do menu
+            Toast.makeText(this, "Projeto Final do Curso - Google Study Jams 2016 - Android for Beginners", Toast.LENGTH_LONG).show(); //Mensagem que aparece ao clicar sobre o menu "SOBRE"
         }
 
         return super.onOptionsItemSelected(item);
